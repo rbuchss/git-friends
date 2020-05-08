e8db8262a48b8d4c0fabe18d5b8190d251e6e7ac
rbuchss <rbuchss@gmail.com>
2020-05-08 07:37:08 -0400

    migrate pre-commit logic to git::hooks::pre_commit

---
+++ git-friends/src/hooks/pre_commit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function git::hooks::pre_commit() {
+  local exit_status=0
+
+  if git diff --cached --quiet --exit-code; then
+    # Nothing to commit so exit
+    return "${exit_status}"
+  fi
+
+  local rules=(.git/hooks/pre-commit-test-*)
+
+  # No rules exist so exit
+  (( "${#rules[@]}" == 0 )) && return "${exit_status}"
+
+  echo 'Running pre-commit tests:'
+
+  for rule in "${rules[@]}"; do
+    "${rule}" || exit_status=$?
+  done
+
+  return "${exit_status}"
+}
