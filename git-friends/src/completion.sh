f74d2e69807cfc38de83b7c3d0993c90e985e67b
rbuchss <rbuchss@gmail.com>
2020-05-08 22:37:40 -0400

    dry out git log alias logic

---
+++ git-friends/src/completion.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function _git_ls() {
+  _git_log
+}
+
+function _git_ll() {
+  _git_log
+}
+
+function _git_ld() {
+  _git_log
+}
+
+function _git_lds() {
+  _git_log
+}
+
+function _git_ldi() {
+  _git_log
+}
---
+++ git-friends/src/log.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function git::log::pretty() {
+  local format \
+    format_options \
+    date_regexp=' (--date[[:space:]=]|--relative-date)'
+
+  format_options=(
+    '%C(bold)%C(yellow)%h'
+    '%C(nobold) %ad'
+    '%C(auto)%d '
+    '%s'
+    '%C(bold)%C(cyan) [%cn]'
+  )
+
+  [[ ! " $* " =~ $date_regexp ]] \
+    && unset 'format_options[1]'
+
+  printf -v format '%s' "${format_options[@]}"
+
+  git log \
+    --graph \
+    --pretty=format:"${format}" \
+    --decorate \
+    "$@"
+}
