74ebe8ce0aac043a37d75db0defb41289ee87938
Russ Buchanan <rbuchss@gmail.com>
2026-02-05 09:33:11 -0500

    add clone cd helpers (#21)

--- git-friends/config/git_aliases.sh
+++ git-friends/config/git_aliases.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # shellcheck source=/dev/null
 source "${HOME}/.git-friends/src/cd.sh"
+source "${HOME}/.git-friends/src/clone.sh"
 source "${HOME}/.git-friends/src/worktree.sh"
 
 # NOTE: these need to be bash aliases vs git ones since git aliases run
 # in a subshell and as such cannot change the parent shell's directory.
 alias gcd='git::cd::root_dir'
+alias gcld='git::clone::cd'
+alias gwcld='git::worktree::clone::cd'
 alias gwco='git::worktree::checkout'
