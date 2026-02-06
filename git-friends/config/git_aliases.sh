68be2989d547df0b6ffd56319f8380a25b6f0e00
Russ Buchanan <rbuchss@gmail.com>
2026-02-06 11:18:35 -0500

    refine git bash aliases (#25)

--- git-friends/config/git_aliases.sh
+++ git-friends/config/git_aliases.sh
@@ -6,7 +6,7 @@ source "${HOME}/.git-friends/src/worktree.sh"
 
 # NOTE: these need to be bash aliases vs git ones since git aliases run
 # in a subshell and as such cannot change the parent shell's directory.
-alias gcd='git::cd::root_dir'
-alias gcld='git::clone::cd'
-alias gwcld='git::worktree::clone::cd'
-alias gwco='git::worktree::checkout'
+alias g-cd='git::cd::root_dir'
+alias g-cl='git::clone::cd'
+alias g-wcl='git::worktree::clone::cd'
+alias g-wco='git::worktree::checkout'
